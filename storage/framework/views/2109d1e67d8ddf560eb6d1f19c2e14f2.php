584a28e1f0037dbba641111e4af4ae611e464172
AhmedMedo <ahmed.alaa@telgani.com>
2025-06-14 23:09:31 +0300

    Apply new .gitignore rules

---
+++ storage/framework/views/2109d1e67d8ddf560eb6d1f19c2e14f2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
+    <title><?php echo e(config('app.name', 'Ecojarah')); ?></title>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+</head>
+<body class="bg-gray-50">
+    <div id="app"></div>
+</body>
+</html><?php /**PATH /home/ahmedalaa/Desktop/eco-jara/project-bolt-sb1-rq8cqfod/project/resources/views/app.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/69dcb9a208e77af5ae0f5517c8531f6c.php
@@ -0,0 +1,8 @@
+<section
+    <?php echo e($attributes->merge(['class' => "@container flex flex-col p-6 sm:p-12 bg-white dark:bg-gray-900/80 text-gray-900 dark:text-gray-100 rounded-lg default:col-span-full default:lg:col-span-6 default:row-span-1 dark:ring-1 dark:ring-gray-800 shadow-xl"])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</section>
+<?php /**PATH /home/ahmedalaa/Desktop/eco-jara/project-bolt-sb1-rq8cqfod/project/vendor/laravel/framework/src/Illuminate/Foundation/Providers/../resources/exceptions/renderer/components/card.blade.php ENDPATH**/ ?>
